b449e8f37f1f2ff912e39c5cfc8d55a80cb0dd44
Greg Ortega <gregorioortegac@gmail.com>
2024-11-30 12:54:20 +1100

    add identity widget

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,27 @@
+"use client"
+
+import { useEffect } from "react"
+const netlifyIdentity = require("netlify-identity-widget")
+
 export default function AdminPage() {
+  useEffect(() => {
+    // Initialize the Netlify Identity widget
+    netlifyIdentity.init()
+
+    // Check for an invite token in the URL
+    const params = new URLSearchParams(window.location.hash.slice(1))
+    const inviteToken = params.get("invite_token")
+
+    if (inviteToken) {
+      // Accept the invite token
+      netlifyIdentity.acceptInvite(inviteToken).then(() => {
+        // Redirect after invite is accepted
+        window.location.hash = "" // Clear the hash
+        window.location.reload() // Reload to initialize the CMS
+      })
+    }
+  }, [])
+
   return (
     <iframe
       src="/admin/index.html"
